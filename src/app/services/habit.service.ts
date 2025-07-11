5223d1bd1d2b8a14e593108ab060b3110eac3a75
MMCCGG <marianocgarcia@hotmail.com>
2025-07-11 13:22:14 +0200

    Rename files and folders

--- src/app/services/habit.service.ts
+++ src/app/services/habit.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class STracking {
+export class HabitService {
 
   constructor() { }
 }
--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class SStorage {
+export class StorageService {
 
   constructor() { }
 }
