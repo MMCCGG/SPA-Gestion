5e93ab3f9cc29a912c5bcf7d536f02a699569338
MMCCGG <marianocgarcia@hotmail.com>
2025-07-11 07:26:50 +0200

    Create components folder

---
+++ src/app/components/habit-card-component/habit-card-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-habit-card-component',
+  imports: [],
+  templateUrl: './habit-card-component.html',
+  styleUrl: './habit-card-component.css'
+})
+export class HabitCardComponent {
+
+}
---
+++ src/app/components/habit-form-component/habit-form-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-habit-form-component',
+  imports: [],
+  templateUrl: './habit-form-component.html',
+  styleUrl: './habit-form-component.css'
+})
+export class HabitFormComponent {
+
+}
